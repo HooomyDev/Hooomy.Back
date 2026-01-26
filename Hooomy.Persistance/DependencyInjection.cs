33850c0cebb2208cd459e07eff5b2fcbbd3add4c
artxmix <artem.artusevskij01@gmail.com>
2026-01-26 16:52:33 +0300

    feat: Add dependency injection for Application and Persistance

---
+++ Hooomy.Persistance/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Hooome.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace Hooome.Persistance;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+        var serverVersion = new MySqlServerVersion(new Version(configuration["DatabaseSettings:ServerVersion"]
+            ?? throw new NullReferenceException("server version was null")));
+
+        services.AddDbContext<HooomeDbContext>(options =>
+        {
+            options.UseMySql(connectionString, serverVersion);
+        });
+
+        services.AddScoped<IHooomeDbContext>(provider =>
+            provider.GetService<HooomeDbContext>()
+                ?? throw new NullReferenceException("provider cant't be null"));
+
+        return services;
+    }
+}
