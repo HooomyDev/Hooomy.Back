9c62d85f52a0aa6cae2fdf5182ce2287405c89ef
artxmix <artem.artusevskij01@gmail.com>
2026-01-26 19:43:39 +0300

    chore: (Hooome.WebApi/Models/CreateRequestDto.cs) Remove status field

--- Hooome.WebApi/Models/CreateRequestDto.cs
+++ Hooome.WebApi/Models/CreateRequestDto.cs
@@ -1,9 +1,7 @@
 ﻿using AutoMapper;
 using Hooome.Application.Common.Mappings;
 using Hooome.Application.Requests.Commands.CreateRequest;
-using Hooome.Application.Requests.Queries.GetRequestList;
 using Hooome.Domain.Enums;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Hooome.WebApi.Models;
 
@@ -11,7 +9,6 @@ public class CreateRequestDto : IMapWith<CreateRequestCommand>
 {
     public string Description { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
-    public RequestStatus Status { get; set; } = RequestStatus.Unknown;
     public RequestCategory Category { get; set; } = RequestCategory.Other;
 
     public void Mapping(Profile profile)
