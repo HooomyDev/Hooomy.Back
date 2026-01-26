f2912f2aefe486bf26b751e177f05a2b7a462bb8
artxmix <artem.artusevskij01@gmail.com>
2026-01-26 17:46:43 +0300

    feat: (Hooome.WebApi/Controllers/RequestController.cs) Add Request controller

---
+++ Hooome.WebApi/Controllers/RequestController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Hooome.Application.Requests.Commands.CreateRequest;
+using Hooome.Application.Requests.Commands.DeleteRequest;
+using Hooome.Application.Requests.Commands.UpdateRequest;
+using Hooome.Application.Requests.Queries.GetRequestDetails;
+using Hooome.Application.Requests.Queries.GetRequestList;
+using Hooome.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hooome.WebApi.Controllers;
+
+[Route("api/requests")]
+public class RequestController(IMapper mapper) : BaseController
+{
+    [HttpGet]
+    public async Task<ActionResult<RequestListVm>> Get()
+    {
+        var query = new GetRequestListQuery
+        {
+            UserId = UserId
+        };
+
+        var vm = await Mediator.Send(query);
+
+        return Ok(vm);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<RequestDetailsVm>> Get(Guid id)
+    {
+        var query = new GetRequestDetailsQuery
+        {
+            UserId = UserId,
+            Id = id
+        };
+
+        var vm = await Mediator.Send(query);
+
+        return Ok(vm);
+    }
+
+    public async Task<ActionResult<Guid>> Create([FromBody] CreateRequestDto dto)
+    {
+        var command = mapper.Map<CreateRequestCommand>(dto);
+        command.UserId = UserId;
+
+        var requestId = await Mediator.Send(command);
+
+        return Ok(requestId);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Update([FromBody] UpdateRequestDto dto)
+    {
+        var command = mapper.Map<UpdateRequestCommand>(dto);
+        command.UserId = UserId;
+    
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var command = new DeleteRequestCommand
+        {
+            Id = id,
+            UserId = UserId
+        };
+
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+}
---
+++ Hooome.WebApi/Models/UpdateRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Hooome.Application.Common.Mappings;
+using Hooome.Application.Requests.Commands.UpdateRequest;
+using Hooome.Domain.Enums;
+
+namespace Hooome.WebApi.Models;
+
+public class UpdateRequestDto : IMapWith<UpdateRequestCommand>
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public RequestCategory Category { get; set; }
+    public RequestStatus Status { get; set; }
+
+    public void Mapping(Profile profile)
+        => profile.CreateMap<UpdateRequestDto, UpdateRequestCommand>();
+}
