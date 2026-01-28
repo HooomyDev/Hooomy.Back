857efc760b90f7fd2e700f51a40954992acdf2da
artxmix <artem.artusevskij01@gmail.com>
2026-01-28 21:30:54 +0300

    chore: (GetRequestListQuery.cs) Separate query and handler to the same files

---
+++ Hooome.Application/Requests/Queries/GetRequestList/GetRequestsListQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Hooome.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hooome.Application.Requests.Queries.GetRequestList;
+
+public class GetRequestsListQueryHandler(IHooomeDbContext dbContext, IMapper mapper) 
+    : IRequestHandler<GetRequestListQuery, RequestListVm>
+{
+    public async Task<RequestListVm> Handle(GetRequestListQuery request, CancellationToken cancellationToken)
+    {
+        var requests = await dbContext.Requests
+            .Where(r => r.UserID == request.UserId)
+            .ProjectTo<RequestListDto>(mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        return new RequestListVm { Requests = requests };
+    }
+}
