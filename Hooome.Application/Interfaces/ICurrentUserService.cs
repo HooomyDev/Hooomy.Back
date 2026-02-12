7b06b62bb60fc900192097008b56b40e3ee74030
artxmix <artem.artusevskij01@gmail.com>
2026-02-13 01:07:09 +0300

    feat: Add CurrentUserService

---
+++ Hooome.Application/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,6 @@
+﻿namespace Hooome.Application.Interfaces;
+
+public interface ICurrentUserService
+{
+    Guid UserId { get; }
+}
---
+++ Hooome.WebApi/Services/CurrentUserService.cs
@@ -0,0 +1,19 @@
+﻿using Hooome.Application.Interfaces;
+using System.Security.Claims;
+
+namespace Hooome.WebApi.Services;
+
+public class CurrentUserService(IHttpContextAccessor accessor)
+    : ICurrentUserService
+{
+    public Guid UserId
+    {
+        get
+        {
+            var id = accessor.HttpContext?.User?
+                .FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return string.IsNullOrEmpty(id) ? Guid.Empty : Guid.Parse(id);
+        }
+    }
+}
