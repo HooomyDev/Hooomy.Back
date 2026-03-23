2209d3a1255429f496e6ade1b2fa1cce704fc290
artxmix <artem.artusevskij01@gmail.com>
2026-03-23 15:11:06 +0300

    feat: Add GetChatForCompany

---
+++ Hooome.Application/Chats/Queries/GetChatForCompany/GetChatForCompanyQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Hooome.Application.Chats.Queries.GetChatForCompany;
+
+public class GetChatForCompanyQuery : IRequest<ChatListForCompanyVm>
+{
+    public Guid CompanyId { get; set; }
+}
---
+++ Hooome.Application/Chats/Queries/GetChatForCompany/GetChatForCompanyQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Hooome.Application.Chats.Queries.GetChatForCompany;
+
+public class GetChatForCompanyQueryValidator 
+    : AbstractValidator<GetChatForCompanyQuery>
+{
+    public GetChatForCompanyQueryValidator()
+    {
+        RuleFor(x => x.CompanyId).NotEqual(Guid.Empty);
+    }
+}
