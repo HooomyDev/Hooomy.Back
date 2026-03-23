9c58df4315504d0e4320ee2d5bd7961240c1a64e
artxmix <artem.artusevskij01@gmail.com>
2026-03-23 15:13:50 +0300

    feat: (ChatController.cs) Add GetChatsForCompanyMethod and userName in create method

--- Hooome.WebApi/Controllers/ChatController.cs
+++ Hooome.WebApi/Controllers/ChatController.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
 using Hooome.Application.Chats.Commands.CreateChat;
 using Hooome.Application.Chats.Queries.GetChatDetailsQuery;
+using Hooome.Application.Chats.Queries.GetChatForCompany;
 using Hooome.Application.Chats.Queries.GetChatList;
 using Hooome.WebApi.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Hooome.WebApi.Controllers;
 
@@ -25,13 +28,13 @@ public class ChatController(IMapper mapper) : BaseController
         return Ok(chats);
     }
 
-    [HttpGet("{companyId:guid}")]
-    public async Task<ActionResult<ChatDetailsVm>> GetDetails(Guid companyId)
+    [HttpGet("{chatId:guid}")]
+    public async Task<ActionResult<ChatDetailsVm>> GetDetails(Guid chatId)
     {
         var query = new GetChatDetailsQuery()
         {
             ResidentId = UserId,
-            CompanyId = companyId
+            ChatId = chatId
         };
 
         var chat = await Mediator.Send(query);
@@ -39,11 +42,29 @@ public class ChatController(IMapper mapper) : BaseController
         return Ok(chat);
     }
 
+    [HttpGet("company-chats/{companyId:guid}")]
+    public async Task<ActionResult<ChatListForCompanyVm>> GetChatsForCompany(Guid companyId)
+    {
+        var query = new GetChatForCompanyQuery()
+        {
+            CompanyId = companyId
+        };
+
+        var chats = await Mediator.Send(query);
+
+        return Ok(chats);
+    }
+
     [HttpPost("create")]
     public async Task<ActionResult<Guid>> Create(CreateChatDto dto)
     {
+        var userName = User.FindFirst(ClaimTypes.Email)?.Value ??
+                   User.FindFirst(ClaimTypes.NameIdentifier)?.Value ??
+                   "Unknown User";
+
         var command = mapper.Map<CreateChatCommand>(dto);
         command.ResidentId = UserId;
+        command.ResidentName = userName;
 
         var chatId = await Mediator.Send(command);
 
