5774b85ae130caf8c0eb6c5709051786bbe7ece1
artxmix <artem.artusevskij01@gmail.com>
2026-04-17 01:28:55 +0300

    feat: Add SoftDeleteRequest command handler
    - Add SoftDeleteRequestCommand with RequestId property
    
    - Add SoftDeleteRequestCommandHandler
    
    - Implement soft delete by setting IsDeleted = true and DeletedAt = UtcNow

---
+++ Hooome.Application/CQRS/Requests/Commands/SoftDeleteRequest/SoftDeleteRequestCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Hooome.Application.CQRS.Requests.Commands.SoftDeleteRequest;
+
+public class SoftDeleteRequestCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+}
---
+++ Hooome.Application/CQRS/Requests/Commands/SoftDeleteRequest/SoftDeleteRequestCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Hooome.Application.CQRS.Requests.Commands.SoftDeleteRequest;
+
+public class SoftDeleteRequestCommandValidator
+    : AbstractValidator<SoftDeleteRequestCommand>
+{
+    public SoftDeleteRequestCommandValidator()
+    {
+        RuleFor(c => c.UserId).NotEqual(Guid.Empty);
+        RuleFor(c => c.Id).NotEqual(Guid.Empty);
+    }
+}
