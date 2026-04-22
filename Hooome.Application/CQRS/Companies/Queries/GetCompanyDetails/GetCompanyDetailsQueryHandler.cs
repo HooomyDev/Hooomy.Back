a8ed2770daaffe1815d0f56df755500a3aa4fac5
artxmix <artem.artusevskij01@gmail.com>
2026-04-22 22:51:11 +0300

    feat: add logo in CompanyDetailsVm

--- Hooome.Application/CQRS/Companies/Queries/GetCompanyDetails/GetCompanyDetailsQueryHandler.cs
+++ Hooome.Application/CQRS/Companies/Queries/GetCompanyDetails/GetCompanyDetailsQueryHandler.cs
@@ -2,11 +2,15 @@
 using Hooome.Application.Common.Exceptions;
 using Hooome.Application.Interfaces;
 using Hooome.Domain;
+using Hooome.Domain.Enums;
 using MediatR;
 
 namespace Hooome.Application.CQRS.Companies.Queries.GetCompanyDetails;
 
-public class GetCompanyDetailsQueryHandler(ICompanyRepository companyRepo, IMapper mapper)
+public class GetCompanyDetailsQueryHandler(ICompanyRepository companyRepo, 
+    ICompanyImageRepository companyImageRepository,
+    IMinioService minioService,
+    IMapper mapper)
     : IRequestHandler<GetCompanyDetailsQuery, CompanyDetailsVm>
 {
     public async Task<CompanyDetailsVm> Handle(GetCompanyDetailsQuery request, CancellationToken cancellationToken)
@@ -14,6 +18,15 @@ public class GetCompanyDetailsQueryHandler(ICompanyRepository companyRepo, IMapp
         var company = await companyRepo.GetById(request.CompanyId, cancellationToken)
             ?? throw new NotFoundException(nameof(Company), request.CompanyId);
 
-        return mapper.Map<CompanyDetailsVm>(company);
+        var companyDetails = mapper.Map<CompanyDetailsVm>(company);
+
+        var companyLogo = await companyImageRepository.GetLogoByCompanyId(companyDetails.Id, cancellationToken);
+
+        if(companyLogo is not null)
+        {
+            companyDetails.LogoUrl = await minioService.GetUrl(ImageType.Company, companyLogo.FileName);
+        }
+
+        return companyDetails;
     }
 }
