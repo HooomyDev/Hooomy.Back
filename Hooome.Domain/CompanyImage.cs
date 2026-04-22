acd6621e2b7d5108b85cad8f31b9463e853f3ad1
artxmix <artem.artusevskij01@gmail.com>
2026-04-22 22:02:33 +0300

    feat(domain): update image entities for MinIO integration
    - Update CompanyImage entity

--- Hooome.Domain/CompanyImage.cs
+++ Hooome.Domain/CompanyImage.cs
@@ -4,11 +4,11 @@ public class CompanyImage
 {
     public Guid Id { get; set; }
     public string FileName { get; set; } = null!;
-    public string FilePath { get; set; } = null!;
+    public string OriginalFileName { get; set; } = null!;
     public long FileSize { get; set; }
-    public bool IsMain { get; set; }
+    public string ContentType { get; set; } = null!;
+    public DateTime UploadedAt { get; set; }
 
     public Guid CompanyId { get; set; }
-
     public Company Company { get; set; } = null!;
 }
