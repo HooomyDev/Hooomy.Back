a0b0034739247fafd562f872f679c8a49b4ad35b
artxmix <artem.artusevskij01@gmail.com>
2026-05-30 13:04:37 +0300

    fix(company): fix the bug related to the company address

--- Hooome.Application/CQRS/Companies/Commands/CreateCompany/CreateCompanyCommandHandler.cs
+++ Hooome.Application/CQRS/Companies/Commands/CreateCompany/CreateCompanyCommandHandler.cs
@@ -5,7 +5,7 @@ using MediatR;
 
 namespace Hooome.Application.CQRS.Companies.Commands.CreateCompany;
 
-public class CreateCompanyCommandHandler(ICompanyRepository companyRepo)
+public class CreateCompanyCommandHandler(ICompanyRepository companyRepo, IAddressRepository addressRepo)
     : IRequestHandler<CreateCompanyCommand, Guid>
 {
     public async Task<Guid> Handle(CreateCompanyCommand request, CancellationToken cancellationToken)
@@ -17,10 +17,18 @@ public class CreateCompanyCommandHandler(ICompanyRepository companyRepo)
             Phone = request.Phone ?? string.Empty,
             Email = request.Email ?? string.Empty,
             WorkingHours = request.WorkingHours ?? string.Empty,
-            AddressId = request.AddressId ?? null,
+            AddressId = request.AddressId,
             CreatedAt = DateTime.UtcNow,
         };
-        
+
+        if (request.AddressId is not null)
+        {
+            var address = await addressRepo.GetById(request.AddressId.Value, cancellationToken)
+                ?? throw new NotFoundException(nameof(Address), request.AddressId.Value);
+
+            address.RegisteredCompanyId = newCompany.Id;
+        }
+
         await companyRepo.Create(newCompany, cancellationToken);
 
         return newCompany.Id;
--- Hooome.Application/CQRS/Companies/Commands/UpdateCompany/UpdateCompanyCommandHandler.cs
+++ Hooome.Application/CQRS/Companies/Commands/UpdateCompany/UpdateCompanyCommandHandler.cs
@@ -6,13 +6,33 @@ using MediatR;
 
 namespace Hooome.Application.CQRS.Companies.Commands.UpdateCompany;
 
-public class UpdateCompanyCommandHandler(ICompanyRepository companyRepo, IMapper mapper)
+public class UpdateCompanyCommandHandler(ICompanyRepository companyRepo, IAddressRepository addressRepo, IMapper mapper)
     : IRequestHandler<UpdateCompanyCommand>
 {
     public async Task Handle(UpdateCompanyCommand request, CancellationToken cancellationToken)
     {
         var company = await companyRepo.GetById(request.Id, cancellationToken)
             ?? throw new NotFoundException(nameof(Company), request.Id);
+
+        var oldAddressId = company.AddressId;
+        if (oldAddressId != request.AddressId)
+        {
+            if (oldAddressId is not null && oldAddressId != Guid.Empty)
+            {
+                var oldAddress = await addressRepo.GetById(oldAddressId.Value, cancellationToken)
+                    ?? throw new NotFoundException(nameof(Address), oldAddressId.Value);
+
+                oldAddress.RegisteredCompanyId = null;
+            }
+
+            if (request.AddressId != Guid.Empty)
+            {
+                var newAddress = await addressRepo.GetById(request.AddressId, cancellationToken)
+                    ?? throw new NotFoundException(nameof(Address), request.AddressId);
+
+                newAddress.RegisteredCompanyId = company.Id;
+            }
+        }
 
         mapper.Map(request, company);
 
